7e26a494e3d301edef8ab5b57e79479c2045d620
Gabi Stanovsky <gabriel.satanovsky@gmail.com>
2021-09-02 20:00:41 +0300

    dataset

---
+++ src/evaluations/dataset_stats.py
@@ -0,0 +1,50 @@
+""" Usage:
+    <file-name> [--in=INPUT_FILE] [--out=OUTPUT_FILE] [--debug]
+
+Options:
+  --help                           Show this message and exit
+  -i INPUT_FILE --in=INPUT_FILE    Input file
+                                   [default: infile.tmp]
+  -o INPUT_FILE --out=OUTPUT_FILE  Input file
+                                   [default: outfile.tmp]
+  --debug                          Whether to debug
+"""
+# External imports
+import logging
+import pdb
+from pprint import pprint
+from pprint import pformat
+from docopt import docopt
+from pathlib import Path
+from tqdm import tqdm
+import numpy as np
+import json
+import pandas as pd
+
+
+# Local imports
+
+
+#----
+
+if __name__ == "__main__":
+
+    # Parse command line arguments
+    args = docopt(__doc__)
+    inp_fn = Path(args["--in"])
+    out_fn = Path(args["--out"])
+
+    # Determine logging level
+    debug = args["--debug"]
+    if debug:
+        logging.basicConfig(level = logging.DEBUG)
+    else:
+        logging.basicConfig(level = logging.INFO)
+
+    logging.info(f"Input file: {inp_fn}, Output file: {out_fn}.")
+
+    # Start computation
+
+    
+    # End
+    logging.info("DONE")
